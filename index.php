447ceb6415853f2ba8c147a18f62ffbe8f106de1
James Mack <jdmack@ucsd.edu>
2013-05-15 21:24:11 +0000

    Finished

--- index.php
+++ index.php
@@ -1,257 +1,193 @@
+<?php
+    date_default_timezone_set("America/Los_Angeles");
+
+    $start_date = "2013-03-31";
+    $end_date = "2013-06-15";
+    $assignment_file = "assignments.txt"; 
+    $quarter_title = "Spring 2013";
+    $days_of_week = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    $quarter_dates = createDateRangeArray($start_date,$end_date);
+    $today_date = new DateTime();
+
+    $assignments = array();
+    $holidays = array();
+?>
 <html>
 <head>
   <title>Spring 2013 Calendar</title>
   <link rel="stylesheet" type="text/css" href="main.css">
   <link rel="shortcut icon" href="favicon.ico" />
 </head>
+<?php
+
+    // INPUT ASSIGNMENTS
+    $handle = fopen($assignment_file, "r");
+    $assignment_pattern = '/^([a-zA-Z0-9 ]+);([a-zA-Z0-9 #]+);([a-zA-Z]+);(\d\d\d\d)-(\d\d)-(\d\d)/';
+    $holiday_pattern = '/^Holiday;([a-zA-Z ]+);(\d\d\d\d)-(\d\d)-(\d\d)/';
+    while($line = fgets($handle)) {
+        if(substr(trim($line), 0, 1) == '#') {
+            continue;
+        }
+        if(preg_match($assignment_pattern, $line, $matches)) {
+        
+            $date_string = "$matches[4]-$matches[5]-$matches[6]";
+            $this_assignment = new Assignment($matches[1], $matches[3], $matches[2], $date_string);
+
+            if(array_key_exists($date_string, $assignments)) {
+                array_push($assignments[$date_string], $this_assignment);
+            }
+            else {
+                $assignments[$date_string] = array($this_assignment);
+            }
+        }
+        else if(preg_match($holiday_pattern, $line, $matches)) {
+            print_r($matches); 
+            $date_string = "$matches[2]-$matches[3]-$matches[4]";
+
+            if(array_key_exists($date_string, $holidays)) {
+                array_push($holidays[$date_string], $matches[1]);
+            }
+            else {
+                $holidays[$date_string] = array($matches[1]);
+            }
+        }
+        else {
+            continue;
+        }
+
+    }
+
+?>
 
 <body background="#000000">
   <center>
     <h1>
-      Spring 2013<br>
+      <?php print "$quarter_title\n"; ?>
+      <br>
     </h1>
 
     <table border="4" align="center">
       <tr>
-        <th class="dayHeader">Sunday</th>
-
-        <th class="dayHeader">Monday</th>
-
-        <th class="dayHeader">Tuesday</th>
-
-        <th class="dayHeader">Wednesday</th>
-
-        <th class="dayHeader">Thursday</th>
-
-        <th class="dayHeader">Friday</th>
-
-        <th class="dayHeader">Saturday</th>
+        <?php
+            // PRINT COLUMN HEADERS
+            foreach($days_of_week as $day) {
+            print "<th class=\"dayHeader\">$day</th>\n";
+            }
+        ?>
       </tr>
+<?php
+   // BUILD CALENDAR
+    $weeks = 0;
+    foreach($quarter_dates as $this_date_str) {
+
+        $this_date = new DateTime($this_date_str);
+        $week_day  = $this_date->format("w");
+        $day       = $this_date->format("j");
+        $month     = $this_date->format("F");
+        $year      = $this_date->format("Y");
+        $padded_day       = $this_date->format("d");
+        $padded_month     = $this_date->format("m");
+        $padded_year      = $this_date->format("Y");
+
+        $date_string = "$padded_year-$padded_month-$padded_day";
+
+        if($week_day == 0) {
+            $weeks++;
+            echo "<tr>\n";
+        }
+
+        if($this_date->format("Y-m-d") < $today_date->format("Y-m-d")) {
+            echo "<td id=\"$date_string\" class=\"past\">";
+        }
+        else if($this_date->format("Y-m-d") == $today_date->format("Y-m-d")) {
+            echo "<td id=\"$date_string\" class=\"today\">";
+        }
+        else {
+            echo "<td id=\"$date_string\">";
+        }
+
+        if($week_day == 0) {
+            echo "<br><br><h2 class=\"week\">Week $weeks</h2>\n";
+        }
+        else {
+            if($day == 1) {
+                echo "<b><span class=\"month\">$month</span> </b>";
+            }
+
+            echo "<b>$day</b><br><br>";
+        }
+
+        // HOLIDAY LOOP
+        if(array_key_exists($date_string, $holidays)) {
+            foreach($holidays[$date_string] as $this_holiday) {
+                print "<span class=\"holiday\"><h3>$this_holiday</h3></span><br>\n";
+            }
+        }
+
+        // ASSIGNMENT LOOP
+        if(array_key_exists($date_string, $assignments)) {
+            foreach($assignments[$date_string] as $this_assignment) {
+                print "<span class=\"$this_assignment->ass_type\">$this_assignment->ass_class: $this_assignment->ass_name</span><br>\n";
+            }
+        }
+
+        echo "</td>\n";
+
+        if($week_day == 6) {
+            echo "</tr>\n";
+        }
+    }
+?>
 
-      <tr>
-        <td class="past"><br><br><h2 class="week">Week 1</h2></td>
-
-        <td class="past"><b><span class="month">April</span> 1</b><br><br></td>
-
-        <td class="past"><b>2<br></b></td>
-
-        <td class="past"><b>3<br></b></td>
-
-        <td class="past"><b>4<br></b></td>
-
-        <td class="past"><b>5<br></b></td>
-
-        <td class="past"><b>6<br></b></td>
-      </tr>
-
-      <tr>
-        <td class="past"><b>7<br><br></b><h2 class="week">Week 2</h2></td>
-
-        <td class="past"><b>8<br><br></b><span class="homework">Math 183: HW #1 due</span><br></td>
-
-        <td class="past"><b>9<br><br></b>
-            <span class="homework">CSE 140: HW #1 due</span><br>
-            <span class="project">CSE 127: Project 1 Part 1 due</span><br>
-        </td>
-
-        <td class="past"><b>10<br><br><br></b></td>
-
-        <td class="past"><b>11</b><br><br><span class="quiz">CSE 131: Quiz 1</span><br></td>
-
-        <td class="past"><b>12<br><br></b></td>
-
-        <td class="past"><b>13<br></b></td>
-      </tr>
-
-      <tr>
-        <td class="past"><b>14</b><br><br><h2 class="week">Week 3</h2></td>
-
-        <td class="past"><b>15<br><br></b>
-            <span class="quiz">CSE 123: Quiz Ch.1</span><br>
-            <span class="homework">Math 183: HW #2 due</span><br>
-        </td>
-
-        <td class="today"><b>16<br><br></b>   
-            <span class="homework">CSE 140: HW #2 due</span><br>
-            <span class="project">CSE 127: Project 1 Part 2 due</span><br>
-        </td>
-
-        <td><b>17<br><br><br><br></b></td>
-
-        <td><b>18<br><br></b></td> 
-
-        <td><b>19<br><br><br><br></b></td>
-
-        <td><b>20<br></b></td>
-      </tr>
-
-      <tr>
-        <td><b>21<br><br></b><h2 class="week">Week 4</h2></td>
-
-        <td><b>22<br><br></b>
-            <span class="test">Math 183: Midterm #1</span><br>
-            <span class="homework">Math 183: HW #3 due</span><br>
-        </td>
-
-        <td><b>23<br><br></b>
-            <span class="homework">CSE 140: HW #3 due</span><br>
-        </td>
-
-        <td><b>24<br><br></b>
-            <span class="homework">CSE 127: HW #1 due</span><br>
-        </td>
-
-        <td><b>25</b><br><br><span class="quiz">CSE 131: Quiz 2</span><br></td>
-
-        <td><b>26<br></b></td>
-
-        <td><b>27<br><br></b></td>
-      </tr>
-
-      <tr>
-        <td><b>28<br><br></b><h2 class="week">Week 5</h2><b><span class="project">CSE 131: Project I due</span><br></b></td>
-
-        <td><b>29<br><br></b>
-            <span class="homework">Math 183: HW #4 due</span><br>
-        </td>
-
-        <td><b>30<br><br><br></b>
-            <span class="homework">CSE 140: HW #4 due</span><br>
-        </td>
-
-        <td><b><span class="month">May</span> 1</b><br><br>
-            <span class="test">CSE 127: Midterm</span><br>
-        </td>
-
-        <td><b>2<br><br></b>
-            <span class="test">CSE 140: Midterm #1</span><br>
-        </td>
-
-        <td><b>3<br><br></b></td>
-
-        <td><b>4<br><br></b></td>
-      </tr>
-
-      <tr>
-        <td><b>5<br><br></b><h2 class="week">Week 6</h2></td>
-
-        <td><b>6<br><br></b>
-            <span class="homework">Math 183: HW #5 due</span><br>
-        </td>
-
-        <td><b>7</b><br><br><span class="quiz">CSE 131: Quiz 3</span><br></td>
-
-        <td><b>8</b><br><br><span class="review">CSE 131: Mid Review</span><br></td>
-
-        <td><b>9</b><br><br><span class="test">CSE 131: Midterm</span><br></td>
-
-        <td><b>10<br><br></b><span class="project">CSE 123: Project Checkpoint</span><br></td>
-
-        <td><b>11<br><br></b></td>
-      </tr>
-
-      <tr>
-        <td><b>12<br><br></b><h2 class="week">Week 7</h2></td>
-
-
-        <td><b>13<br><br></b>
-            <span class="homework">Math 183: HW #6 due</span><br>
-        </td>
-
-        <td><b>14<br><br></b>
-            <span class="homework">CSE 140: HW #5 due</span><br>
-        </td>
-
-        <td><b>15<br><br></b></td>
-
-        <td><b>16<br><br></b></td>
-
-        <td><b>17<br><br></b></td>
-
-        <td><b>18<br><br></b></td>
-      </tr>
-
-      <tr>
-        <td><b>19<br><br></b><h2 class="week">Week 8</h2></td>
-
-        <td><b>20<br><br></b>
-            <span class="test">Math 183: Midterm #1</span><br>
-            <span class="homework">Math 183: HW #7 due</span><br>
-        </td>
-
-        <td><b>21<br><br><br>
-            <span class="homework">CSE 140: HW #6 due</span><br>
-        </b></td>
-
-        <td><b>22<br><br><br></b></td>
-
-        <td><b>23</b><br><br>
-            <span class="quiz">CSE 131: Quiz 4</span><br>
-            <span class="test">CSE 140: Midterm #2</span><br>
-        </td>
-
-        <td><b>24<br><br></b></td>
-
-        <td><b>25<br><br><br><br></b></td>
-      </tr>
-
-      <tr>
-        <td><b>26<br><br></b><h2 class="week">Week 9</h2></td>
-
-        <td><b>27<br><br>Holiday<br></b></td> 
-
-        <td><b>28<br><br></b></td>
-
-        <td><b>29<br><br></b>
-            <span class="homework">Math 183: HW #8 due</span><br>
-        </td>
-
-        <td><b>30<br><br><br><br></b></td>
-
-        <td><b>31<br><br></b></td>
-
-        <td><b><span class="month">June</span> 1<br><br></b></td>
-      </tr>
-
-      <tr>
-        <td><b>2<br><br></b><h2 class="week">Week 10</h2></td>
-
-        <td><b>3<br><br></b></td>
-
-        <td><b>4<br><br><br></b>
-            <span class="homework">CSE 140: HW #7 due</span><br>
-        </td>
-
-        <td><b>5<br><br></b>
-            <span class="homework">Math 183: HW #9 due</span><br>
-        </td>
-
-        <td><b>6</b><br><br><span class="quiz">CSE 131: Quiz 5</span><br></td>
-
-        <td><b>7<br><br></b><span class="project">CSE 131: Project II due</span></b></td>
-
-        <td><b>8<br><br><br><br></b></td>
-      </tr>
-
-      <tr>
-        <td><b>9<br></b><br><h2 class="week">Finals Week</h2><span class="review">CSE 131: Final Review</span><br></td>
-
-        <td><b>10</b><br><br><span class="test">CSE 131: Final Exam</span><br>11:30a-2:30p<br>TBA<br></td>
-
-        <td><b>11</b><br><br><span class="test">CSE 140: Final Exam</span><br>3:00p - 6:00p<br>Center 115</td>
-
-        <td><b>12</b><br><br>
-            <span class="test">CSE 123: Final Exam</span><br>8:00a - 11:00a<br>TBA</br>
-            <span class="test">Math 183: Final Exam</span><br>3:00p - 6:00p<br>Center 115</br>
-        </td>
-
-        <td><b>13</b><br><br><br></b></td>
-
-        <td><b>14</b><br><br><span class="test">CSE 127: Final Exam</span><br>7:00p - 9:59<br>TBA</td>
-
-        <td><b>15<br><br><br><br></b></td>
-      </tr>
     </table>
   </center>
 </body>
 </html>
+
+
+
+<?php
+    function createDateRangeArray($strDateFrom,$strDateTo)
+    {
+        $aryRange=array();
+
+        $iDateFrom=mktime(1,0,0,substr($strDateFrom,5,2),     substr($strDateFrom,8,2),substr($strDateFrom,0,4));
+        $iDateTo=mktime(1,0,0,substr($strDateTo,5,2),     substr($strDateTo,8,2),substr($strDateTo,0,4));
+
+        if ($iDateTo>=$iDateFrom)
+        {
+            array_push($aryRange,date('Y-m-d',$iDateFrom)); // first entry
+            while ($iDateFrom<$iDateTo)
+            {
+                $iDateFrom+=86400; // add 24 hours
+                array_push($aryRange,date('Y-m-d',$iDateFrom));
+            }
+        }
+        return $aryRange;
+    }
+
+
+    class Assignment
+    {
+        public $ass_class;
+        public $ass_type;
+        public $ass_name;
+        public $ass_date;
+
+        function __construct($cl, $ty, $na, $da) {
+            $this->ass_class = $cl;
+            $this->ass_type  = $ty;
+            $this->ass_name  = $na;
+            $this->ass_date  = $da;
+        }
+
+        function getHTMLString()
+        {
+            return "<span class=\"$type\">$class: $name</span><br>\n";
+        }
+    }
+
+
+
+
+?>
